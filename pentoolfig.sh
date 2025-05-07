9bff015047a09eb8bef2a22e4b5d33688f237873
DamClover <114771411+damclover@users.noreply.github.com>
2025-05-07 20:04:23 -0300

    Update pentoolfig.sh

--- pentoolfig.sh
+++ pentoolfig.sh
@@ -38,7 +38,7 @@ tools=('nmap' 'nikto' 'sqlmap' 'whatweb' 'gobuster' 'ffuf' 'dirb' 'dirbuster'
        'netcat-openbsd' 'tcpdump' 'masscan' 'enum4linux' 'smbclient' 
        'smbmap' 'ldap-utils' 'feroxbuster' 'sslscan' 'wfuzz' 'xsser' 'recon-ng'
        'seclists' 'wordlists' 'wafw00f' 'subfinder' 'iptables' 'proxychains4' 'tor'
-       'searchsploit')
+       'searchsploit' 'nuclei')
 
 # Install the listed tools
 for tool in "${tools[@]}"; do
